037dba6b0f5d6ea52c4b71bad44c433504a5b6fc
Bruna G <BrunaGagli@BrGagliardi.local>
2018-11-20 21:22:30 -0200

    Initial commit

---
+++ app/src/main/java/com/app/project/kotlinmesseger/views/ChatItem.kt
@@ -0,0 +1,38 @@
+package com.app.project.kotlinmesseger.views
+
+import com.app.project.kotlinmesseger.models.User
+import com.squareup.picasso.Picasso
+import com.xwray.groupie.Item
+import com.xwray.groupie.ViewHolder
+import kotlinx.android.synthetic.main.chat_from_row.view.*
+import kotlinx.android.synthetic.main.chat_to_row.view.*
+import com.app.project.kotlinmesseger.R
+
+class ChatFromItem(val text: String, val user: User): Item<ViewHolder>() {
+    override fun bind(viewHolder: ViewHolder, position: Int) {
+        viewHolder.itemView.textview_chat_from_row.text = text
+
+        val uri = user.profileImageUrl
+        val targetImageView = viewHolder.itemView.imageView_chat_from_row
+        Picasso.get().load(uri).into(targetImageView)
+    }
+
+    override fun getLayout(): Int {
+        return R.layout.chat_from_row
+    }
+}
+
+class ChatToItem(val text: String, val user: User): Item<ViewHolder>() {
+    override fun bind(viewHolder: ViewHolder, position: Int) {
+        viewHolder.itemView.textview_chat_to_row.text = text
+
+        // load our user image into the star
+        val uri = user.profileImageUrl
+        val targetImageView = viewHolder.itemView.imageView_chat_to_row
+        Picasso.get().load(uri).into(targetImageView)
+    }
+
+    override fun getLayout(): Int {
+        return R.layout.chat_to_row
+    }
+}
